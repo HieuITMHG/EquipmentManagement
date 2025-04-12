f7f00e86e3e9dcf11d3024a180e2e9d0175ba22a
HieuITMHG <danghuuhieu038@gmail.com>
2025-04-12 08:57:30 +0700

    authorization

--- EquipmentManagement/blueprints/student.py
+++ EquipmentManagement/blueprints/student.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, render_template, request, session, redirect, flash
 
-from helpers.helpers import login_required, get_expect_returning_time
+from helpers.helpers import login_required, get_expect_returning_time, role_required
 from services.student_service import StudentService
 from services.room_service import RoomService
 from services.equipment_service import EquipmentService
 from services.borrow_service import BorrowService
 from services.penalty_service import PenaltyService
+from enums.role_type import RoleID
 
 student_blueprint = Blueprint('student', __name__)
 
 @student_blueprint.route('/student', methods=['GET'])
+
 @login_required
+@role_required(RoleID.STUDENT.value)
 def student_dashboard():
     if request.method == "GET":
         student_id = session.get('account_id')
@@ -33,19 +36,20 @@ def student_dashboard():
     
 @student_blueprint.route('/student/borrow', methods=['POST'])
 @login_required
+@role_required(RoleID.STUDENT.value)
 def student_borrow():
     if request.method == "POST":
         lst_item = request.form.getlist('items')  
         student_id = session.get("account_id") 
         room_id = request.form.get("room_id")
         if not lst_item:
-            flash("Vui lòng chọn ít nhất một thiết bị!", "danger")
+            flash("Vui lòng chọn ít nhất một thiết bị!", "error")
             return redirect('/student')
 
         expect_returning_time = get_expect_returning_time()
 
         if not expect_returning_time:
-            flash("Không thể mượn thiết bị ngoài khung giờ quy định!", "danger")
+            flash("Không thể mượn thiết bị ngoài khung giờ quy định!", "error")
             return redirect('/student')
 
         equipment_ids = ",".join(lst_item)  
@@ -56,17 +60,18 @@ def student_borrow():
             if borrow_request_id:
                 flash(f"✅ Mượn thiết bị thành công! Mã yêu cầu: {borrow_request_id}", "success")
             else:
-                flash("❌ Lỗi khi mượn thiết bị!", "danger")
+                flash("❌ Lỗi khi mượn thiết bị!", "error")
         else:
             if BorrowService.update_borrow_request(equipment_ids, existing_request[0]["id"]):
                 flash("Cập nhật yêu cầu mượn thiết bị thành công!", "success")
             else:
-                flash("Lỗi khi cập nhật yêu cầu mượn thiết bị!", "danger")
+                flash("Lỗi khi cập nhật yêu cầu mượn thiết bị!", "error")
 
         return redirect('/student')
     
 @student_blueprint.route('/student/profile', methods=['GET'])
 @login_required
+@role_required(RoleID.STUDENT.value)
 def student_profile():
     if request.method == "GET":
         student = StudentService.get_student_by_id(session.get('account_id'))
@@ -74,6 +79,7 @@ def student_profile():
     
 @student_blueprint.route('/student/borrow_history', methods=['GET', 'POST'])
 @login_required
+@role_required(RoleID.STUDENT.value)
 def borrow_history():
     student_id = session.get('account_id')  
     student = StudentService.get_student_by_id(student_id)
@@ -87,6 +93,7 @@ def borrow_history():
     return render_template('student/borrow_history.html', lst_borrow=lst_borrow, student=student)
 
 @student_blueprint.route('/student/violation', methods=['GET'])
+@role_required(RoleID.STUDENT.value)
 @login_required
 def violation():
     student_id = session.get('account_id')  
